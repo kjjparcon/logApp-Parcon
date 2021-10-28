ef4b01aa15a1b1a646d03acd9138c749cc8020a4
kjjparcon <201980240@psu.palawan.edu.ph>
2021-10-29 02:48:59 +0800

    updated

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', 'http://localhost/logApp-Parcon');
-	define('DB_HOST', 'localhost');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6447446');
 	define('DB_PASS', 'RFi9iEXbgG');
 	define('DB_NAME', 'sql6447446');
