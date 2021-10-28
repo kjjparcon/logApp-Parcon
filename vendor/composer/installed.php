597d4195b7ef3fd6b2c2e9a6e20fd46f32df206b
kjjparcon <91300563+kjjparcon@users.noreply.github.com>
2021-10-29 03:06:10 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ef4b01aa15a1b1a646d03acd9138c749cc8020a4',
+        'name' => 'kjjparcon/logapp-parcon',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kjjparcon/logapp-parcon' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ef4b01aa15a1b1a646d03acd9138c749cc8020a4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
