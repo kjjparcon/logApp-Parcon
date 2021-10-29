bab5f1354bbe2a7efe7d5c7e7199cfc40bd9b7fd
kjjparcon <91300563+kjjparcon@users.noreply.github.com>
2021-10-29 17:10:22 +0800

    updated

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost/logApp-Parcon');
+	define('ROOT_URL', 'https://mighty-gorge-01751.herokuapp.com/');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6447446');
 	define('DB_PASS', 'RFi9iEXbgG');
